8c3ba4cf8d0e728871187095a5054dabd1bb2b30
andrejmanin <andrejmanin54@gmail.com>
2024-05-15 18:07:22 +0300

    HM commit

--- main.cpp
+++ main.cpp
@@ -4,55 +4,34 @@
 using namespace std;
 
 void task_1() {
-    int year;
-    cin >> year;
-    cout << (year % 4 == 0 && ((year % 100 != 0) || (year % 400 == 0))) ? "366 days" : "365 days";
+    int secconds = 0;
+    cin >> secconds;
+    cout << "hours: " << secconds / 3600 << "\nminutes: " << secconds / 60 << "\nsecconds: " << secconds << endl;
 }
 
 void task_2() {
-    int grn, cop;
-    cin >> grn >> cop;
-    cout << "You have " << grn + cop / 100 << "grn and " << cop % 100 << "cop" << endl;
+    float num = 0;
+    cin >> num;
+    cout << static_cast<int>(num) << " grn. " << (num - static_cast<int>(num)) * 100 << " cop." << endl;
 }
 
 void task_3() {
-    cout << "Calculation of the volume of the parallelepiped." << endl;
-    cout << "Enter the output data:" << endl;
-    cout << "Lenth (cm) -> ";
-    double len, width, height;
+    cout << "Calculation of running speed.\nEnter the length of the distance (meters) = ";
+    int len = 0, t = 1;
     cin >> len;
-    cout << "Width (cm) -> ";
-    cin >> width;
-    cout << "Height (cm) -> ";
-    cin >> height;
-    cout << "Volume: " << len * width * height << " cm3" << endl;
+    cout << "Enter the time(min.sec) = ";
+    cin >> t;
+    cout << "Distance: " << len << endl;
+    cout << "Time: " << t * 60 << " c" << endl;
+    cout << "You ran with speed: " << len / (t * 60) << " m/c" << endl;
 }
 
 void task_4() {
-    cout << "Calculation of the distance between settlements. \nEnter the output data: " << endl;
-    cout << "Map scale(number of kilometers in one centimeter) -> ";
-    double len, l;
-    cin >> len;
-    cout << "Distance between points representing settlements (cm) -> ";
-    cin >> l;
-    cout << "Distance between settlements " << len * l << " km" << endl;
-}
-
-void task_5() {
-    double pi = 3.14, R;
-    cin >> R;
-    cout << (4 / 3) * pi * pow(R, 3) << endl;
+    int days;
+    cin >> days;
+    cout << "weeks: " << days / 7 << " days: " << days - (days / 7);
 }
 
 int main() {
-    //cout << "Task 1" << endl;
-    //task_1();
-    //cout << "Task 2" << endl;
-    //task_2();
-    //cout << "Task 3" << endl;
-    //task_3();
-    //cout << "Task 4" << endl;
-    //task_4();
-    cout << "Task 5" << endl;
-    task_5();
+    task_4();
 }
